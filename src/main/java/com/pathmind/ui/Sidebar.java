1c66c37455855e1b38f1f6f59acb37cadbdb1f8c
ryder <ryderjt1@icloud.com>
2025-10-21 12:22:28 -0600

    Update Sidebar.java

--- src/main/java/com/pathmind/ui/Sidebar.java
+++ src/main/java/com/pathmind/ui/Sidebar.java
@@ -119,7 +119,7 @@ public class Sidebar {
         columnsUsed = Math.min(columnsUsed, TAB_COLUMNS);
 
         if (columnsUsed > 1) {
-            currentInnerSidebarWidth = INNER_SIDEBAR_WIDTH * 2 - 16;
+            currentInnerSidebarWidth = INNER_SIDEBAR_WIDTH * 2 - 6;
         } else {
             currentInnerSidebarWidth = INNER_SIDEBAR_WIDTH;
         }
